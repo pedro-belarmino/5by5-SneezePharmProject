193a0edf0ed8893a1def5a60ca4b829cce0cca24
G-ERICA <ericagf21@gmail.com>
2025-10-23 12:21:31 -0300

    Classe

---
+++ Application/Customers.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application
+{
+    public class Customers
+    {
+        public string Cpf { get; private set; }
+        public string Nome { get; private set; }
+        public DateOnly DataNascimento { get; private set; }
+        public string Telefone { get; private set; }
+        public DateOnly UltimaCompra { get; private set; }
+        public DateOnly DataCadastro { get; private set; }
+        public char Situacao { get; private set; }
+
+
+        public Customers(string cpf, string nome, DateOnly dataNascimento, string telefone, DateOnly ultimaCompra, DateOnly dataCadastro, char situacao)
+            {
+                Cpf = cpf;
+                Nome = nome;
+                DataNascimento = dataNascimento;
+                Telefone = telefone;
+                UltimaCompra = ultimaCompra;
+                DataCadastro = dataCadastro;
+                Situacao = situacao;
+            }
+        
+
+        public bool ValidarCPF(string cpf) 
+        {
+            
+        }
+
+
+
+    }
+}
---
+++ Application/RestrictedCustomers.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application
+{
+    public class RestrictedCustomers
+    {
+        public Customers Customers {  get; set; }
+
+
+        public RestrictedCustomers(Customers customers) 
+        {
+            Customers = customers;
+        }
+
+
+    }
+}
