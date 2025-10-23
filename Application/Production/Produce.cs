cbdb6f3b34d2b2dbf23cc4eb7864adaf2e66c88d
Henrique <pro.henriquerossin@gmail.com>
2025-10-23 12:14:36 -0300

    feat add classes in Production

--- Application/Production/Produce.cs
+++ Application/Production/Produce.cs
@@ -8,5 +8,17 @@ namespace Application.Prod
 {
     public class Produce
     {
+        public int Id { get; set; }
+        public DateOnly DataProducao { get; set; }
+        public string Medicamento { get; set; }
+        public int Quantidade { get; set; }
+
+        public Produce(int id, DateOnly dataProducao, string medicamento, int quantidade)
+        {
+            Id = id;
+            DataProducao = dataProducao;
+            Medicamento = medicamento;
+            Quantidade = quantidade;
+        }
     }
 }
--- Application/Production/ProduceItem.cs
+++ Application/Production/ProduceItem.cs
@@ -8,5 +8,17 @@ namespace Application.Prod
 {
     public class ProduceItem
     {
+        Ingredient ingredient { get; set; }
+        public int Id { get; set; }
+        public string Principio { get; set; }
+        public int QuantidadePrincipio { get; set; }
+
+        public ProduceItem(Ingredient ingredient, int id, Ingredient principio, int quantidadePrincipio)
+        {
+            this.ingredient = ingredient;
+            Id = id;
+            Principio = ingredient.Id;
+            QuantidadePrincipio = quantidadePrincipio;
+        }
     }
 }
