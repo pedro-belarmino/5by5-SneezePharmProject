0b4d496e7138883286a9526605a27945f5571063
pedro-5by5 <pedro.gbelarmino@gmail.com>
2025-10-24 15:21:13 -0300

    estabilidando versões

---
+++ Application/Classes/Sales/SalesItens.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application
+{
+    public class SalesIten
+    {
+        public int Id { get; private set; }
+        public int Quantidade { get; private set; }
+        public decimal ValorUnitario { get; private set; }
+        public double TotalItem { get; private set; }
+        public int CDB { get; private set; }
+
+        public SalesIten(int i, int q, decimal v, double t, int cdb)
+        {
+            this.Id = i;
+            this.Quantidade = q;
+            this.ValorUnitario = v;
+            this.TotalItem = t;
+            this.CDB = cdb;
+        }
+    }
+}
