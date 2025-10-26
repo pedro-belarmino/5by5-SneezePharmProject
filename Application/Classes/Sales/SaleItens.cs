af253efaa7fbb12c544baecc097e4ceb9f862473
pedro-5by5 <pedro.gbelarmino@gmail.com>
2025-10-26 15:42:11 -0300

    asd

--- Application/Classes/Sales/SaleItens.cs
+++ Application/Classes/Sales/SaleItens.cs
@@ -3,26 +3,45 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Application.Utils.WritersAndReaders;
 
 namespace Application
 {
     public class SaleItens
     {
-        public int Id { get; private set; }
-        public int Quantidade { get; private set; }
-        public decimal ValorUnitario  { get; private set; }
-        public int TotalItem { get; private set; }
-        //public Medicine medicamento = new Medicine();
+        Writer_Reader objeto = new();
 
-        public SaleItens(int i, int q, decimal v
-            //, Medicine m
-            )
+        public List<SaleItens> SaleItensList = new();
+        public string Id { get; private set; }
+        public int Quantidade { get; private set; }
+        public decimal ValorUnitario { get; private set; }
+        public int TotalItem { get; private set; }
+        public string CDBMedicamento { get; private set; }
+        private int lastId = 0;
+
+        static string diretorio = "C:\\Projects\\5by5-SneezePharmProject\\Application\\Diretorios\\";
+        static string file = "SaleItens.data";
+
+        string fullPath = Path.Combine(diretorio, file);
+
+        public SaleItens()
+        {
+            objeto.Verificador(diretorio, fullPath);
+
+        }
+
+        public SaleItens(string i, int q, decimal v, string m, int t)
         {
             this.Id = i;
             this.Quantidade = q;
-            this.ValorUnitario = v; 
-            //this.TotalItem = m;
-            //this.Medicine.CDB;
+            this.ValorUnitario = v;
+            this.TotalItem = t;
+            this.CDBMedicamento = m;
         }
+
+
+
+
+
     }
 }
